d50584584300942c0c980d3d26d6c30f50d4aaff
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-05-13 03:18:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer();
 app.get('/', function(req, res) {
-    res.send('test\n');
+    res.send('test v1\n');
 });
 
 module.exports = app;
